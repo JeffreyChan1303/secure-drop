c5ed6e50e07b46663907a8e4dd2a7f418c6aa237
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-10-04 23:24:02 -0400

    added secure-drop.py main application loop, interactive shell interface

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # secure-drop
 Intro to Computer Security Project
+
+### setup
+
+1. clone repository
+2. run python script by inputing the commands below
+```
+python3 secure-drop.py
+```
+
+#### Team Members
+
+* Jeffrey Chan
+* Christian
+* Ben
--- secure-drop.py
+++ secure-drop.py
@@ -1,6 +1,36 @@
 import src
 
-src.userRegistration()
+
+def main():
+  # login to application
+
+  # src.userRegistration()
+
+
+  # Shell loop
+  print("\n\nWelcome to Secure Drop.")
+  print("Type 'help' for commands.\n")
+
+  while True:
+    command = input("$secure_drop> ")
+
+    if command == "help":
+      print("'add' -> Add a new contact")
+      print("'list' -> List all online contacts")
+      print("'send' -> Transfer file to contact")
+      print("'exit' -> Exit SecureDrop\n")
+    elif command == "add":
+      pass
+    elif command == "list":
+      pass
+    elif command == "send":
+      pass
+    elif command == "exit":
+      return
+    else:
+      print(f"'{command}' is not a valid command.")
+      
+main()
 
 
 
