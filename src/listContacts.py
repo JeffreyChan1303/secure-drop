63d4fa5e2c2933b163680a7e3d8eca979f3ba606
Justarski <72470988+Justarski@users.noreply.github.com>
2022-11-28 17:40:23 -0500

    Add files via upload

--- src/listContacts.py
+++ src/listContacts.py
@@ -5,8 +5,7 @@ from time import sleep
 def listContacts(userEmail):
   UDPsocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) ## AF_INET is family of protocols. SOCK_DGRAM is a type that for connectionless protocols
   port = 25555
-  # msg = b"(Broadcast message): Please respond if you are online."
-  msg = f"Looking for file transfer,{userEmail}".encode("utf-8")
+  msg = "List Request".encode("utf-8")
   UDPsocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
   UDPsocket.sendto(msg, ("255.255.255.255",port))
 
@@ -16,7 +15,9 @@ def listContacts(userEmail):
   # here print out the nearby users
   with open("./data/nearbyUsers.json", "r") as fp:
     nearbyUsers= json.load(fp)
-    print("\n Email | IP Address | Port \n")
+    print("\n Email | IP Address | Port")
+    if len(nearbyUsers) == 0:
+      print("No Contacts Online.\n")
     for email in nearbyUsers:
       print(email, "|", nearbyUsers[email]['ip'],"|", nearbyUsers[email]['port'])
 
--- src/udpserver.py
+++ src/udpserver.py
@@ -4,7 +4,7 @@ import json
 '''
 This is a background thread that runs and continuously picks broadcasts and thier respective ip adresses
 '''
-def udpserver(userEmail):
+def udpserver(userEmail, stopThreads):
   nearbyUsers = {}
   ## AF_INET is family of protocols. SOCK_DGRAM is a type that for connectionless protocols
   UDPsocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -15,21 +15,38 @@ def udpserver(userEmail):
   UDPsocket.bind((host,port)) # binds address:(hostname,port#) to socket 
 
   while True:
-    data,addr = UDPsocket.recvfrom(1024) # this method receives UDP message , 1024 means the # of bytes to be read from the udp socket.
-    msg, email = data.decode("utf-8").split(",")
+    if not stopThreads:
+      print("running")
+      data,addr = UDPsocket.recvfrom(1024) # this method receives UDP message , 1024 means the # of bytes to be read from the udp socket.
+      msg = data.decode("utf-8").split(",")
+      print(msg)
 
-    # if another user requests LIST, it will run this block of code
-    if msg == "Looking for file transfer":
-      if email not in nearbyUsers:
-        print("Device Reply")
-        UDPsocket.sendto(f"Looking for file transfer,{userEmail}".encode('utf-8'), addr)
-      nearbyUsers[email] = {
-        "ip": addr[0],
-        "port": addr[1],
-      }
+      # if another user sends "List Request", it will send "List Reply"
+      if msg[0] == "List Request":
+        print(f"Received a 'List Request' from {addr}")
+        UDPsocket.sendto(f"List Reply,{userEmail}".encode('utf-8'), (addr[0], 25555))
 
-      with open("./data/nearbyUsers.json", "w") as fp:
-        json.dump(nearbyUsers, fp, indent=2)
+      # if another user sends "List Request" and receives "List Reply", it will run this block of code
+      if msg[0] == "List Reply":
+        print("Received a 'List Reply'")
+        with open("./data/contacts.json", "r") as Cfp:
+          allContacts = json.load(Cfp)
+          if userEmail in allContacts:
+            userContacts = allContacts[userEmail]
+          else:
+            userContacts = {}
+          if msg[1] in userContacts:
+            nearbyUsers[msg[1]] = {
+            "ip": addr[0],
+            "port": addr[1],
+            } 
 
-    if data.decode() == "quit":
+        with open("./data/nearbyUsers.json", "w") as fp:
+          json.dump(nearbyUsers, fp, indent=2)
+
+      if data.decode() == "quit":
+        break
+
+    else:
+      print("Stopped threads.\n")
       break
