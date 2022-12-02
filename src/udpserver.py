66f73424eee7d36dc72b803ada385fe1b54df85e
Justin Tarnowski <Justarski99@gmail.com>
2022-12-02 18:24:41 -0500

    fixed udpserver

--- src/udpserver.py
+++ src/udpserver.py
@@ -16,6 +16,8 @@ def udpserver(userEmail):
   UDPsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   host = ''
   port = 25555 # UDP port number
+  userHostName = socket.gethostname()
+  userIP = socket.gethostbyname(userHostName)
   UDPsocket.bind((host,port)) # binds address:(hostname,port#) to socket 
 
   # listening for "List Request"
@@ -25,7 +27,7 @@ def udpserver(userEmail):
     print(msg)
 
     # if another user sends "List Request", it will send "List Reply"
-    if msg[0] == "List Request":
+    if msg[0] == "List Request" and addr[0] is not userIP:
       print(f"Received a 'List Request' from {addr}")
 
       # start tcp client and transfer email
