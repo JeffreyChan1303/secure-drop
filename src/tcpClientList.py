529a9ec441e6122c133fcf95faa4b3f05ffe1610
SamuelVilt <samuelgvilt@gmail.com>
2022-12-09 17:38:03 -0500

    certs update

--- src/tcpClientList.py
+++ src/tcpClientList.py
@@ -1,33 +1,38 @@
 import socket
 import json
+import ssl
 
 
 # establish tcp connection with specific host and port number
 def tcpClientList(userEmail, targetIP):
+    context=ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context = ssl._create_unverified_context()
+    context.load_verify_locations("certs\pki\issued\samuelvilt.crt")
     # sets up the options and address for the TCP socket
-    TCPsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    TCPsocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    host = targetIP
-    port = 25565
-    TCPsocket.connect((host,port))
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as TCPsocket:
+        TCPsocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        host = targetIP
+        port = 25565
+        TCPsocket.connect((host,port))
+        ssock = context.wrap_socket(TCPsocket, server_hostname = "localhost")
 
-    print(f"Sent a 'List Reply' to ({host}, {port})")
-    TCPsocket.send(bytes(f"List Reply,{userEmail}", "utf-8"))
+        print(f"Sent a 'List Reply' to ({host}, {port})")
+        ssock.send(bytes(f"List Reply,{userEmail}", "utf-8"))
 
-    # receive the "List Request #2"
-    msg = TCPsocket.recv(1024)
-    msg = msg.decode("utf-8").split(",")
+        # receive the "List Request #2"
+        msg = ssock.recv(1024)
+        msg = msg.decode("utf-8").split(",")
 
-    # check if the other user's email is in this user's contacts, if it is, then send a confirmation message
-    if msg[0] == "List Request #2":
-        print(f"Received a 'List Request #2' from {host} (ADD PORT)")
-        with open("./data/contacts.json", "r") as Cfp:
-            allContacts = json.load(Cfp)
-            if msg[1] in allContacts:
-                print(f"Sent a 'Both Contacts Verified' to ({host}, {port})")
-                TCPsocket.send(bytes("Both Contacts Verified", "utf-8"))  
-                TCPsocket.close()
-            else:
-                print(f"Sent a 'Contact Not Verifeid' to ({host}, {port})")
-                TCPsocket.send(bytes("Contact Not Verified", "utf-8"))
-                TCPsocket.close()
+        # check if the other user's email is in this user's contacts, if it is, then send a confirmation message
+        if msg[0] == "List Request #2":
+            print(f"Received a 'List Request #2' from {host} (ADD PORT)")
+            with open("./data/contacts.json", "r") as Cfp:
+                allContacts = json.load(Cfp)
+                if msg[1] in allContacts:
+                    print(f"Sent a 'Both Contacts Verified' to ({host}, {port})")
+                    ssock.send(bytes("Both Contacts Verified", "utf-8"))  
+                    ssock.close()
+                else:
+                    print(f"Sent a 'Contact Not Verifeid' to ({host}, {port})")
+                    ssock.send(bytes("Contact Not Verified", "utf-8"))
+                    ssock.close()
--- src/tcpServerList.py
+++ src/tcpServerList.py
@@ -1,11 +1,13 @@
 import socket
 import json
-
+import ssl
 
 # open tcp server on a specific host, and port number.
 def tcpServerList(userEmail):
     print("TCP STARTED")
     emailReply = ""
+    context=ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain("certs/pki/issued/ca.crt","certs/pki/private/ca.key", 'secure-dropSJJ')
 
     # sets up the options and address for the TCP socket
     TCPsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
@@ -17,46 +19,47 @@ def tcpServerList(userEmail):
     TCPsocket.settimeout(4)
 
     # listening for "List Reply" or "Both contacts verified"
-    while True:
-        print("TCP server listening at ", TCPsocket.getsockname())
-        try:
-            server, addr = TCPsocket.accept()
-        except TimeoutError:
-            print("Timeout for tcpServerList")
-            return
-        msg = server.recv(1024)
-        msg = msg.decode("utf-8").split(",")
+    with context.wrap_socket(TCPsocket,server_side=True) as ssock:
+        while True:
+            print("TCP server listening at ", ssock.getsockname())
+            try:
+                server, addr = ssock.accept()
+            except TimeoutError:
+                print("Timeout for tcpServerList")
+                return
+            msg = server.recv(1024)
+            msg = msg.decode("utf-8").split(",")
 
-        # if the message is "List Reply"
-        if msg[0] == "List Reply":
-            print(f"Received a 'List Reply' from ({addr[0]}, {addr[1]})")
-            emailReply = msg[1]
-            with open("./data/contacts.json", "r") as Cfp:
-                allContacts = json.load(Cfp)
-                if emailReply in allContacts:
-                    print(f"Sent a 'List Request #2' to ({addr[0]}, {port})")
-                    server.send(bytes(f"List Request #2,{userEmail}", "utf-8"))
-                else:
-                    print(f"Sent a 'Contact Not Verified' to ({addr[0]}, {port})")
-                    server.send(bytes(f"Contact Not Verified", "utf-8"))
-        
-        msg = server.recv(1024)
-        msg = msg.decode("utf-8")
+            # if the message is "List Reply"
+            if msg[0] == "List Reply":
+                print(f"Received a 'List Reply' from ({addr[0]}, {addr[1]})")
+                emailReply = msg[1]
+                with open("./data/contacts.json", "r") as Cfp:
+                    allContacts = json.load(Cfp)
+                    if emailReply in allContacts:
+                        print(f"Sent a 'List Request #2' to ({addr[0]}, {port})")
+                        server.send(bytes(f"List Request #2,{userEmail}", "utf-8"))
+                    else:
+                        print(f"Sent a 'Contact Not Verified' to ({addr[0]}, {port})")
+                        server.send(bytes(f"Contact Not Verified", "utf-8"))
+            
+            msg = server.recv(1024)
+            msg = msg.decode("utf-8")
 
-        # if the message is "Both contacts verified"
-        if msg == "Both Contacts Verified":
-            print("Received a 'Both Contacts Verified'...Closing TCP server")
-            with open("./data/nearbyContacts.json", "r") as NCfpR:
-                nearbyContacts = json.load(NCfpR)
-                with open("./data/nearbyContacts.json", "w") as NCfpW:
-                    with open("./data/contacts.json", "r") as Cfp:
-                        contacts = json.load(Cfp)
-                        nearbyContacts[emailReply] = {
-                            "fullName": contacts[userEmail][emailReply]["fullName"],
-                            "ip": addr[0]
-                        }
-                        json.dump(nearbyContacts, NCfpW)
+            # if the message is "Both contacts verified"
+            if msg == "Both Contacts Verified":
+                print("Received a 'Both Contacts Verified'...Closing TCP server")
+                with open("./data/nearbyContacts.json", "r") as NCfpR:
+                    nearbyContacts = json.load(NCfpR)
+                    with open("./data/nearbyContacts.json", "w") as NCfpW:
+                        with open("./data/contacts.json", "r") as Cfp:
+                            contacts = json.load(Cfp)
+                            nearbyContacts[emailReply] = {
+                                "fullName": contacts[userEmail][emailReply]["fullName"],
+                                "ip": addr[0]
+                            }
+                            json.dump(nearbyContacts, NCfpW)
 
-        # if the message is "Contact not verified"
-        if msg == "Contact Not Verified":
-            print("Received a 'Contact Not Verified'...Closing TCP server")
+            # if the message is "Contact not verified"
+            if msg == "Contact Not Verified":
+                print("Received a 'Contact Not Verified'...Closing TCP server")
